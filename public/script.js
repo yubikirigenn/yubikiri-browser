51e0fcfc16c321eda65250a61dff155e7c28e9b2
yubikirigenn <yubikirigenn@gmail.com>
2025-09-18 21:02:04 +0900

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,26 +1,20 @@
 const form = document.getElementById('proxyForm');
-const result = document.getElementById('result');
+const shadowContent = document.querySelector('#result').shadowRoot.querySelector('#shadowContent');
 
 form.addEventListener('submit', async (e) => {
   e.preventDefault();
   let url = document.getElementById('url').value.trim();
 
-  if (!url) {
-    alert('URLを入力してください');
-    return;
-  }
+  if (!url) url = 'https://' + url;
+  else if (!/^https?:\/\//i.test(url)) url = 'https://' + url;
 
-  if (!/^https?:\/\//i.test(url)) {
-    url = 'https://' + url;
-  }
-
-  result.innerHTML = "読み込み中...";
+  shadowContent.innerHTML = "読み込み中...";
 
   try {
     const res = await fetch(`/proxy?url=${encodeURIComponent(url)}`);
     const html = await res.text();
-    result.innerHTML = html;
+    shadowContent.innerHTML = html;
   } catch (err) {
-    result.innerHTML = "エラー: " + err.message;
+    shadowContent.innerHTML = "エラー: " + err.message;
   }
 });
