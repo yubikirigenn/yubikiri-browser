f4780df97fef156dd2e864ccc24404c05244438f
yubikirigenn <yubikirigenn@gmail.com>
2025-09-18 21:13:19 +0900

    Delete public/shadow-loader.js

--- public/shadow-loader.js
+++
@@ -1,4 +0,0 @@
-// #result を Shadow DOM に置き換える
-const result = document.getElementById('result');
-const shadowRoot = result.attachShadow({ mode: 'open' });
-shadowRoot.innerHTML = '<div id="shadowContent"></div>';
