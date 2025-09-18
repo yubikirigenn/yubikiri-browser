dfb764c863ec7586a74c87b87471471baefd64ec
yubikirigenn <yubikirigenn@gmail.com>
2025-09-18 15:24:01 +0900

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 import express from "express";
 import fetch from "node-fetch";
-import cheerio from "cheerio";
+import * as cheerio from "cheerio"; // ← 修正！
 import path from "path";
 import { fileURLToPath } from "url";
 import { URL } from "url";
@@ -11,15 +11,12 @@ const __dirname = path.dirname(__filename);
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// 静的ファイル
 app.use(express.static(path.join(__dirname, "public")));
 
-// index.html
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "views", "index.html"));
 });
 
-// プロキシ (HTML 書き換え付き)
 app.get("/proxy", async (req, res) => {
   const targetUrl = req.query.url;
   if (!targetUrl) {
@@ -33,9 +30,8 @@ app.get("/proxy", async (req, res) => {
     // HTML の場合だけ書き換え
     if (contentType.includes("text/html")) {
       const html = await response.text();
-      const $ = cheerio.load(html);
+      const $ = cheerio.load(html); // そのまま使える
 
-      // 相対リンクを書き換える
       const rewriteAttr = (selector, attr) => {
         $(selector).each((_, el) => {
           const val = $(el).attr(attr);
@@ -58,7 +54,7 @@ app.get("/proxy", async (req, res) => {
       res.setHeader("Content-Type", "text/html");
       res.send($.html());
     }
-    // CSS の場合 url(...) を書き換え
+    // CSS の場合
     else if (contentType.includes("text/css")) {
       let css = await response.text();
       css = css.replace(/url\((.*?)\)/g, (match, p1) => {
@@ -71,7 +67,7 @@ app.get("/proxy", async (req, res) => {
       res.setHeader("Content-Type", "text/css");
       res.send(css);
     }
-    // 画像・JS などはそのまま転送
+    // その他
     else {
       const buffer = Buffer.from(await response.arrayBuffer());
       res.setHeader("Content-Type", contentType);
