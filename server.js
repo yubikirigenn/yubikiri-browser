776a9c228119bec1cef61beeacf57994679f5091
yubikirigenn <yubikirigenn@gmail.com>
2025-09-19 12:53:47 +0900

    Update server.js

--- server.js
+++ server.js
@@ -1,20 +1,20 @@
-const express = require("express");
-const path = require("path");
-const proxyRouter = require("./proxy");
+const express = require('express');
+const path = require('path');
+const proxyRouter = require('./proxy');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 // 静的ファイル
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // ルートページ
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "views", "index.html"));
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'index.html'));
 });
 
 // プロキシ
-app.use("/proxy", proxyRouter);
+app.use('/proxy', proxyRouter);
 
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
